34a992026e6cdf3b999c35cfa3aa0aef611e5e41
enc0ded <66119874+enc0ded@users.noreply.github.com>
2020-06-05 00:19:08 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -14,7 +14,7 @@ Config = {
 }
 
 -- delete vehicle entities that no player is close to, also deletes game spawned npc vehicles. It's practically a more agressive garbage collecter. This could, maybe, might, should, probably improves client performance. 
-Config.entityManagement = true
+Config.entityManagement = false -- experimental
 
 -- the distance a vehicle entity needs to be from all players to be deleted. 
 Config.entityManagementDistance = Config.respawnDistance + 50 -- must be higher than 350!
